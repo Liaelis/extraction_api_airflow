d53b63f8c647cb84979a8a0e0cc6eee54458499a
elis regina <er.elisscherer@gmail.com>
2023-08-15 16:27:37 -0300

    config first test with spark local for transformation data

--- airflow_pipeline/dags/twitter_dag.py
+++ airflow_pipeline/dags/twitter_dag.py
@@ -3,7 +3,7 @@ sys.path.append("airflow_pipeline")
 
 from airflow import DAG
 from datetime import datetime,timedelta
-
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from os.path import join
 from airflow.utils.dates import days_ago
 from operators.twitter_operator import TwitterOperator
@@ -11,8 +11,16 @@ from operators.twitter_operator import TwitterOperator
 with DAG(dag_id = "TwitterDAG", start_date=days_ago(2),schedule_interval="@daily") as dag:
     query = "data science"
 
-    to=TwitterOperator(file_path=join("datalake/twitter_datascience",
+    twitter_operator=TwitterOperator(file_path=join("datalake/twitter_datascience",
                                         "extract_data={{ ds }}",
                                         "datascience_{{ ds_nodash }}.json"),
                          query=query, start_time="{{ data_interval_start.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}", 
                          end_time="{{ data_interval_end.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}", task_id="twitter_datascience")
+    twitter_transform = SparkSubmitOperator(task_id="transform_twitter_datascience",
+                                            application="/home/lia/Documents/airflow/src/spark/transformation.py",
+                                            name="twittter transformation",
+                                            application_args=["--src", "/home/lia/Documents/airflow/datalake/twitter_datascience",
+                                                              "--dest","/home/lia/Documents/airflow/data_transformate",
+                                                              "--process_date", "{{ds}}"]
+                                          )
+    twitter_operator >> twitter_transform
